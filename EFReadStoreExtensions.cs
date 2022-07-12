5271d1478e618877e19aa877e23a6f7bfa93c218
Robert Thomson <robert.thomson@screenmedia.co.uk>
2022-07-12 16:26:40 +0100

    Files for fix

---
+++ EFReadStoreExtensions.cs
@@ -0,0 +1,26 @@
+﻿using EventFlow;
+using EventFlow.EntityFramework.ReadStores;
+using EventFlow.Extensions;
+using EventFlow.ReadStores;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyEventFlow.ReadStore;
+
+public static class EFReadStoreExtensions
+{
+    public static IEventFlowOptions UseEntityFrameworkCoreReadModel<TReadModel, TDbContext>(
+        this IEventFlowOptions eventFlowOptions)
+        where TDbContext : DbContext
+        where TReadModel : class, IReadModel, new()
+    {
+        return eventFlowOptions
+            .RegisterServices(f =>
+            {
+                f.Register<IEntityFrameworkReadModelStore<TReadModel>,
+                    EntityFrameworkCoreReadModelStore<TReadModel, TDbContext>>();
+                f.Register<IReadModelStore<TReadModel>>(r =>
+                    r.Resolver.Resolve<IEntityFrameworkReadModelStore<TReadModel>>());
+            })
+            .UseReadStoreFor<IEntityFrameworkReadModelStore<TReadModel>, TReadModel>();
+    }
+}
---
+++ ReadStoreModule.cs
@@ -0,0 +1,19 @@
+﻿using EventFlow;
+using EventFlow.Configuration;
+using EventFlow.EntityFramework;
+using EventFlow.EntityFramework.Extensions;
+using EventFlow.Extensions;
+using EventFlow.MsSql.Extensions;
+
+namespace MyEventFlow.ReadStore;
+
+public class ReadStoreModule : IModule
+{
+    public void Register(IEventFlowOptions eventFlowOptions)
+    {
+        eventFlowOptions.ConfigureEntityFramework(EntityFrameworkConfiguration.New)
+            .AddDefaults(typeof(ReadStoreModule).Assembly)
+            .AddDbContextProvider<MyReadStoreDbContext, MyReadStoreDbContextProvider>()
+            .UseEntityFrameworkCoreReadModel<MyReadModel, MyReadStoreDbContext>();
+    }
+}
